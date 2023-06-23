e55824bafe6e80d6835ad1d7c243f7b457168cc6
rakshran <78072886+rakshran@users.noreply.github.com>
2023-06-23 08:31:49 +0530

    header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import logo from '../images/pawduck_logo.png'
+import logo from '/pawduck_logo.png'
 
 const Header = ({ siteTitle }) => (
   <header
