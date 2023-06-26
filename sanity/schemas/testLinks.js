5ab34a6d2bc7b23b7907f9d9b94f23e682826cfa
rakshran <78072886+rakshran@users.noreply.github.com>
2023-06-26 14:39:11 +0530

    Create testLinks.js

---
+++ sanity/schemas/testLinks.js
@@ -0,0 +1,18 @@
+// schemas/testLinks.js
+
+export default {
+  name: "testLinks",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+    },
+    {
+      name: "url",
+      type: "string",
+      title: "URL",
+    },
+  ],
+};
