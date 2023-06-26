47f71b4d4c497981538e0bb3c43ced5af7cb5376
rakshran <78072886+rakshran@users.noreply.github.com>
2023-06-26 21:36:13 +0530

    Update test.js

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { useSanity } from "sanity";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
 
 const TestPage = () => {
   const { data } = useSanity();
   const links = data.collections.testLinks.filter((link) => link.title);
 
   return (
+    <Layout>
     <ul>
       {links.map((link) => (
         <li key={link._id}>
@@ -13,6 +18,7 @@ const TestPage = () => {
         </li>
       ))}
     </ul>
+    </Layout>
   );
 };
 
