0eadc9800d4d65dda23dc3ba9ebc75944a09e70a
rakshran <78072886+rakshran@users.noreply.github.com>
2023-06-27 13:55:11 +0530

    Update schema.js

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -1,6 +1,6 @@
 export default {
     name: 'techArticle',
-    title: 'Tech Article',
+    title: 'Tech Articles',
     type: 'document',
     fields: [
       {
@@ -12,6 +12,126 @@ export default {
         name: 'url',
         title: 'URL',
         type: 'url',
+      },
+        {
+        name: 'section',
+        title: 'Section',
+        type: 'string',
+      },
+    ],
+  };
+
+export default {
+    name: 'businessArticle',
+    title: 'Business Articles',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'url',
+        title: 'URL',
+        type: 'url',
+      },
+        {
+        name: 'section',
+        title: 'Section',
+        type: 'string',
+      },
+    ],
+  };
+
+export default {
+    name: 'designArticle',
+    title: 'Design Articles',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'url',
+        title: 'URL',
+        type: 'url',
+      },
+        {
+        name: 'section',
+        title: 'Section',
+        type: 'string',
+      },
+    ],
+  };
+
+export default {
+    name: 'productivityArticle',
+    title: 'Productivity Articles',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'url',
+        title: 'URL',
+        type: 'url',
+      },
+        {
+        name: 'section',
+        title: 'Section',
+        type: 'string',
+      },
+    ],
+  };
+
+export default {
+    name: 'careerArticle',
+    title: 'Career Articles',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'url',
+        title: 'URL',
+        type: 'url',
+      },
+        {
+        name: 'section',
+        title: 'Section',
+        type: 'string',
+      },
+    ],
+  };
+
+export default {
+    name: 'toolsArticle',
+    title: 'Tools Articles',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'url',
+        title: 'URL',
+        type: 'url',
+      },
+        {
+        name: 'section',
+        title: 'Section',
+        type: 'string',
       },
     ],
   };
