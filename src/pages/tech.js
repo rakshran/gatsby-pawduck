21445a09004a212cee26c69b1044fd99faa17d5c
rakshran <78072886+rakshran@users.noreply.github.com>
2023-06-29 01:46:37 +0530

    Sanity

--- src/pages/tech.js
+++ src/pages/tech.js
@@ -6,7 +6,7 @@ import Seo from "../components/seo";
 
 
 const TechPage = ({ data }) => {
-  const articles = data.allTech.edges;
+  const articles = data.allSanityTech.edges;
 
   return (
     <Layout>
@@ -26,7 +26,7 @@ const TechPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allTech {
+    allSanityTech {
       edges {
         node {
         _id
