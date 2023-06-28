ce741d1be3bccf3c344bdafd9f4502bbfa855824
rakshran <78072886+rakshran@users.noreply.github.com>
2023-06-29 01:28:41 +0530

    Create schema.js

---
+++ sanity/schemas/schema.js
@@ -0,0 +1,20 @@
+export default {
+    name: 'business',
+    title: 'Business',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+      },
+      {
+        name: 'URL',
+        type: 'url',
+      },
+      {
+        name: 'section',
+        type: 'string',
+      }
+    ],
+  }
+ 
