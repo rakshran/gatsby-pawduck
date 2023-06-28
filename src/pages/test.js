3ba5f455a029fec668da596020cc1b7ec1887260
rakshran <78072886+rakshran@users.noreply.github.com>
2023-06-28 11:44:36 +0530

    Create test.js

---
+++ src/pages/test.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+const TechPage = ({ data }) => {
+  // Collect articles by section
+  const articlesBySection = data.allSanityTech.edges.reduce((sections, edge) => {
+    const { section, title, url } = edge.node;
+
+    if (!sections[section]) {
+      sections[section] = [];
+    }
+
+    sections[section].push({ title, url });
+    return sections;
+  }, {});
+
+  return (
+    <Layout>
+      <Seo title="Tech" />
+      <h1>Tech</h1>
+      <p>Fundamentals of Computer Science.</p>
+      {Object.entries(articlesBySection).map(([section, articles]) => (
+        <div key={section}>
+          <h2>{section}</h2>
+          <ul>
+            {articles.map((article) => (
+              <li key={article.title}>
+                <a href={article.url}>{article.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allSanityTech {
+      edges {
+        node {
+          _id
+          title
+          url
+          section
+        }
+      }
+    }
+  }
+`;
+
+export default TechPage;
